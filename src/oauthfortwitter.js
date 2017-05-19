6f56620c60a466c1927b264beec4644b3982f7ee
Youngki Kang <yellowgg2@gmail.com>
2017-05-20 00:25:39 +0900

    need to fix

--- src/oauthfortwitter.js
+++ src/oauthfortwitter.js
@@ -17,29 +17,29 @@ exports.OAuthForTwitter = class OAuthForTwitter {
      'https://api.twitter.com/', null, 'oauth2/token', null);
     oauth2.getOAuthAccessToken('', {
       'grant_type': 'client_credentials'
-    }, function (e, access_token) {
+    }, function (e, access_token, refresh_token, results) {
       console.log("====     " + access_token); //string that we can use to authenticate request
 
-      var options = {
-        hostname: 'api.twitter.com',
-        path: '/1.1/statuses/user_timeline.json?screen_name=mostlyharmlessd',
-        headers: {
-          Authorization: 'Bearer ' + access_token
-        }
-      };
+    //   var options = {
+    //     hostname: 'api.twitter.com',
+    //     path: '/1.1/statuses/user_timeline.json?screen_name=mostlyharmlessd',
+    //     headers: {
+    //       Authorization: 'Bearer ' + access_token
+    //     }
+    //   };
 
 
-      https.get(options, function (result) {
-        var buffer = '';
-        result.setEncoding('utf8');
-        result.on('data', function (data) {
-          buffer += data;
-        });
-        result.on('end', function () {
-          var tweets = JSON.parse(buffer);
-          console.log(tweets); // the tweets!
-        });
-      });
+    //   https.get(options, function (result) {
+    //     var buffer = '';
+    //     result.setEncoding('utf8');
+    //     result.on('data', function (data) {
+    //       buffer += data;
+    //     });
+    //     result.on('end', function () {
+    //       var tweets = JSON.parse(buffer);
+    //       console.log(tweets); // the tweets!
+    //     });
+    //   });
     });
   }
 }
